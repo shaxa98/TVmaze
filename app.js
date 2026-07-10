b5a254eda835f836e76b072782ec33c5596b8bc0
shaxa98 <shakhzodakhrorov@gmail.com>
2026-07-10 08:11:23 +0500

    update array

--- app.js
+++ app.js
@@ -9,36 +9,88 @@
 
 // console.log(mujiklar[3]);
 
-var arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
-// Hamma sonlarni 2 ga kupaytir
-// sonlarKvadrati iborat yangi array oching
-// numberlar yig'indisi chiqar
-let kvadrat = [];
-for (let son = 0; son < arr.length; son++) {
-  kvadrat.push(arr[son] * 2);
-  console.log(kvadrat[son]);
-}
+// var arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
+// // Hamma sonlarni 2 ga kupaytir
+// // sonlarKvadrati iborat yangi array oching
+// // numberlar yig'indisi chiqar
+// let kvadrat = [];
+// for (let son = 0; son < arr.length; son++) {
+//   kvadrat.push(arr[son] * 2);
+//   console.log(kvadrat[son]);
+// }
 
-let students = [
-  "Ali",
-  "Aziz",
-  "Madina",
-  "Bekzod",
-  "Dilshod",
-  "Avaz",
-  "Donyor",
-  "Samandar",
-  "Shahzod",
-  "Abdulboqi",
-  "Shaxzod",
-  "Salohiddin",
-];
-// Studentlar ichidan Samandarni top
-// Student Samandarni index indexini aniqla
+// let students = [
+//   "Ali",
+//   "Aziz",
+//   "Madina",
+//   "Bekzod",
+//   "Dilshod",
+//   "Avaz",
+//   "Donyor",
+//   "Samandar",
+//   "Shahzod",
+//   "Abdulboqi",
+//   "Shaxzod",
+//   "Salohiddin",
+// ];
+// // Studentlar ichidan Samandarni top
+// // Student Samandarni index indexini aniqla
 
-for (let index = 0; index < students.length; index++) {
-  const student = students[index];
-  if (student == "Samandar") {
-    console.log("Student Samandar topildi " + index + " indexda");
-  }
-}
+// for (let index = 0; index < students.length; index++) {
+//   const student = students[index];
+//   if (student == "Samandar") {
+//     console.log("Student Samandar topildi " + index + " indexda");
+//   }
+// }
+
+const mevalar = ["Olma", "Anor", "Banan"];
+mevalar.sort();
+console.log(mevalar); // ['Anor', 'Banan', 'Olma']
+
+// const sonlar =;
+
+// // O'sish tartibida:
+// sonlar.sort((a, b) => a - b);
+// console.log(sonlar); // [2, 5, 10]
+
+// // Kamayish tartibida:
+// sonlar.sort((a, b) => b - a);
+// console.log(sonlar); // [10, 5, 2]
+
+// const odamlar = [
+//   { ism: 'Ali', yosh: 25 },
+//   { ism: 'Vali', yosh: 20 }
+// ];
+
+// // Yoshi bo'yicha o'sish tartibida:
+// odamlar.sort((a, b) => a.yosh - b.yosh);
+// console.log(odamlar); // Kichigi birinchi chiqadi
+
+// const harflar = ['A', 'B', 'C', 'D'];
+// harflar.reverse();
+
+// console.log(harflar); // ['D', 'C', 'B', 'A']
+
+// const mevalar = ['Olma', 'Anor', 'Banan'];
+
+// // Oldin saralanadi, keyin teskari qilinadi
+// mevalar.sort().reverse();
+
+// console.log(mevalar); // ['Olma', 'Banan', 'Anor']
+
+// const original =;
+// const teskari = original.reverse();
+
+// console.log(original); // [3, 2, 1] <-- Asl massiv ham o'zgardi!
+// console.log(teskari);  // [3, 2, 1]
+
+// const original =;
+
+// // 1-usul: Spread operatori
+// const nusxa1 = [...original].reverse();
+
+// // 2-usul: Zamonaviy .toReversed() metodi (Asl massivga tegmaydi)
+// const nusxa2 = original.toReversed();
+
+// console.log(original); // [1, 2, 3] <-- Asl massiv o'zgarmadi
+// console.log(nusxa2);   // [3, 2, 1]
